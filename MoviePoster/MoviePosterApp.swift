f8165cbd286a3c0c4c941500fc113caec4d93fba
Oncu <oohancan@gmail.com>
2022-08-04 14:47:23 +0300

    Initial Commit

---
+++ MoviePoster/MoviePosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePosterApp.swift
+//  MoviePoster
+//
+//  Created by Oncu Can on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviePosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
